a715f086f13f5697026301dcc56634b82e516eec
Antoine (AntoDev) <antoine@rivrs.io>
2024-05-24 19:20:51 +0200

    fix ContextTest unit test

--- src/test/kotlin/ContextTest.kt
+++ src/test/kotlin/ContextTest.kt
@@ -30,15 +30,15 @@ class ContextTest {
 
                 val context = useContext(TestContext)
 
-                assertEquals(1, this.index)
-                assertEquals(1, context.index)
+                assertEquals(2, this.index)
+                assertEquals(2, context.index)
 
             }
 
             val context = useContext(TestContext)
 
-            assertEquals(0, this.index)
-            assertEquals(0, context.index)
+            assertEquals(1, this.index)
+            assertEquals(1, context.index)
 
         }
 
@@ -51,7 +51,7 @@ class ContextTest {
 
         val context = useContext(TestContext)
 
-        assertEquals(0, context.index)
+        assertEquals(1, context.index)
 
     }
 
